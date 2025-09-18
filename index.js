8e790d99134dff0f01e4cfc4b8c043974fa9f8f2
mayragpttest <mayragpttest@gmail.com>
2025-09-18 16:12:56 -0700

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ app.get("/scrape", async (req, res) => {
     const browser = await puppeteer.launch({
       args: ["--no-sandbox", "--disable-setuid-sandbox"]
     });
-
     const page = await browser.newPage();
     await page.goto(url, { waitUntil: "networkidle0" });
 
